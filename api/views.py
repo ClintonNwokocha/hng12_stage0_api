3b0697ca83d44fa1d6d336654c5a9571dec3e4b2
Clinton <leoclinton2011@hotmail.com>
2025-01-30 15:19:18 +0100

    Updated the views.py to what's expected

--- api/views.py
+++ api/views.py
@@ -4,9 +4,11 @@ from datetime import datetime
 import pytz
 
 def get_info(request):
+    current_datetime = datetime.now(pytz.utc).replace(microsecond=0).isoformat()
+    current_datetime = current_datetime + "Z"
     data = {
             "email": "leoclinton2011@hotmail.com",
-            "current_datetime": datetime.now(pytz.utc).isoformat(),
+            "current_datetime": current_datetime,
             "github_url": "https://github.com/ClintonNwokocha/hng12_stage0_api"
             }
     return JsonResponse(data, json_dumps_params={'indent': 4})
